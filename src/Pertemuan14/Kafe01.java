1b1f4cddb6705125f48aa251295d3c11cb3baa21
MamankSYQ <abdurrahmansyafiq151123@gmail.com>
2025-11-26 09:38:16 +0700

    Percobaan 2

--- src/Pertemuan14/Kafe01.java
+++ src/Pertemuan14/Kafe01.java
@@ -1,7 +1,7 @@
 package Pertemuan14;
 public class Kafe01 {
-    public static void Menu() {
-        System.out.println("===== MENU RESTO KAFE =====");
+    public static void main (String[] args){
+         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
@@ -10,8 +10,5 @@ public class Kafe01 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
-        }
-    public static void main (String[] args){
-        Menu();
     }
 }
